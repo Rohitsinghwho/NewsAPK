e21da391801102a25693011d0af6890c2911e8ff
Rohit Singh <rohiisingh8655@gmail.com>
2023-07-17 14:26:13 +0530

    Added a card layout using bootstrap

--- src/components/NewsComp.js
+++ src/components/NewsComp.js
@@ -1,6 +1,33 @@
 import React, { Component } from "react";
+import DemoImg from "./Demoimg.jpg";
 export default class NewsComp extends Component {
   render() {
-    return <div>hello i am a news component</div>;
+    //intitalizing props
+    let { title, description, imgUrl, urlToNews } = this.props;
+    return (
+      <div className="container my-5">
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={
+              !imgUrl ? DemoImg : imgUrl
+            } /*Ternery operator for img as some of them are null*/
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a
+              href={urlToNews}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary btn-sm"
+            >
+              Read More..
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }

45dbc87ee8d4196d893e43ecbda77b159d67ef31
Rohit Singh <rohiisingh8655@gmail.com>
2023-07-17 14:24:20 +0530

    modified app .js and added navbar with some extra specification

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import NewsItems from "./components/NewsItems";
+import Alert from "./components/Alert";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-
-        <NewsItems />
+        <Alert />
+        <h1 className="text-center my-4">NEWS APK- TOP HEADLINES</h1>
+        <NewsItems pageSize={6} />
       </div>
     );
   }

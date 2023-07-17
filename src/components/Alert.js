76820f08953a596a98978df2bc65644a44d6121a
Rohit Singh <rohiisingh8655@gmail.com>
2023-07-17 14:28:41 +0530

    created a alert component yet to use

---
+++ src/components/Alert.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Alert extends Component {
+  render() {
+    return (
+      <div>
+        <div className="alert alert-primary" role="alert">
+          A simple primary alert—check it out!
+        </div>
+      </div>
+    );
+  }
+}

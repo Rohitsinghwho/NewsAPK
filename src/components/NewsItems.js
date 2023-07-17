e42e7ae089c2f7220d853a3d3a0caf5d940255cc
Rohit Singh <rohiisingh8655@gmail.com>
2023-07-17 12:44:50 +0530

    Created a News Item componenet to display news

---
+++ src/components/NewsItems.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import NewsComp from "./NewsComp";
+
+export default class NewsItems extends Component {
+  render() {
+    return (
+      <div>
+        <NewsComp />
+      </div>
+    );
+  }
+}
